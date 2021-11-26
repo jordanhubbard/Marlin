bd43c1837b391610aa3deecf164a04c9b716d828
Jordan Hubbard <jordanhubbard@gmail.com>
2021-11-26 11:54:12 -0800

    Try to stick git commit hash information into the version string so that I don't go crazy trying to associate a given Marlin FW version in the printer with the source code that actually built it (I don't know if this will work).

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,13 +28,13 @@
 /**
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "2.0.9.2"
+#define SHORT_BUILD_VERSION "2.0.9.2-$Id$"
 
 /**
  * Verbose version identifier which should contain a reference to the location
  * from where the binary was downloaded or the source code was compiled.
  */
-//#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION
+#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION
 
 /**
  * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
@@ -54,7 +54,7 @@
  * has a distinct Github fork— the Source Code URL should just be the main
  * Marlin repository.
  */
-//#define SOURCE_CODE_URL "github.com/MarlinFirmware/Marlin"
+#define SOURCE_CODE_URL "http://github.com/jordanhubbard/Marlin.git"
 
 /**
  * Default generic printer UUID.
