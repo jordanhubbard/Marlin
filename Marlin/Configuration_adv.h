1420fb8365e4a555b83dd0058264620578e3df40
Jordan Hubbard <jordanhubbard@gmail.com>
2022-01-16 20:01:12 -0800

    There we go!  Had to define the non-AUTO pin to -1 before it would like this.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -584,9 +584,11 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E1_AUTO_FAN_PIN PB5
-#define E2_AUTO_FAN_PIN PB6
-#define E3_AUTO_FAN_PIN PB7
+#define E0_AUTO_FAN_PIN PB7
+#define FAN_PIN -1
+#define E1_AUTO_FAN_PIN PB6
+#define E2_AUTO_FAN_PIN -1
+#define E3_AUTO_FAN_PIN -1
 #define E4_AUTO_FAN_PIN -1
 #define E5_AUTO_FAN_PIN -1
 #define E6_AUTO_FAN_PIN -1
