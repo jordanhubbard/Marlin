d49e323e326d4d7c04199fd0538e43844af366fe
Jordan Hubbard <jordanhubbard@gmail.com>
2022-01-21 10:45:37 -0800

    Name this properly.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -144,7 +144,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender 5+ Wanker"
+#define CUSTOM_MACHINE_NAME "Wanker"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
